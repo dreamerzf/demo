6cfd060a64b2a76528e7e71e3332322e28843e46
dreamerzf <zengief@163.com>
2020-06-16 11:11:42 +0800

    跨域  执行顺序:corsFilter 拦截器  addCorsMapping

--- src/main/java/com/zf/demo/config/Interceptor/InterceptorConfig.java
+++ src/main/java/com/zf/demo/config/Interceptor/InterceptorConfig.java
@@ -1,8 +1,14 @@
 package com.zf.demo.config.Interceptor;
 
 import com.zf.demo.utils.CommonUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,4 +31,39 @@ public class InterceptorConfig implements WebMvcConfigurer {
         List<String> list=Arrays.asList(excludePath.split(","));
         registry.addInterceptor(myInterceptor).addPathPatterns("/**").excludePathPatterns(list);
     }
+
+    /**
+     * addMapping在拦截器之后执行，不能解决跨域问题
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("POST","GET","OPTIONS","DELETE")
+                .maxAge(1800)
+                .allowCredentials(true);
+    }
+    private CorsConfiguration getCorsConfig(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(1800L);
+        return corsConfiguration;
+    }
+
+    /**
+     * 使用过滤器解决跨域问题，在拦截器之前执行
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",getCorsConfig());
+        return new CorsFilter(source);
+    }
+
 }
--- src/main/java/com/zf/demo/config/Interceptor/MyInterceptor.java
+++ src/main/java/com/zf/demo/config/Interceptor/MyInterceptor.java
@@ -1,11 +1,16 @@
 package com.zf.demo.config.Interceptor;
 
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import redis.clients.jedis.JedisCluster;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 /**
  * Create by zengfei
@@ -14,13 +19,46 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class MyInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private JedisCluster jedisCluster;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("执行到preHandle");
+        /**
+         * 如果拦截之后想返回前台一个标志status
+         * 如果不加过滤器的时候跨域第一次发送的Option报文放过
+         */
+        String userId=request.getHeader("userId");
+        if(HttpMethod.OPTIONS.toString().equals(request.getMethod())){
+            response.setStatus(HttpServletResponse.SC_OK);
+            return true;
+        }
+        if(userId!=null && userId.length()>0){
+            String value=jedisCluster.get(userId);
+            if(value==null){
+                PrintWriter writer=response.getWriter();
+                JSONObject jsonObject=new JSONObject();
+                jsonObject.put("loginstatus","9999");//该userId超时
+                jsonObject.put("loginuser",userId);
+                writer.append(jsonObject.toString());
+//                response.sendRedirect(request.getContextPath()+"/login/index");//返回标志和重定向任选其一
+                return false;  //此处一定要是return false 不然会有write的错误
+            }else{
+                jedisCluster.expire(userId,120);//更新缓存时间
+                return true;
+            }
+        }else {
+            PrintWriter writer = response.getWriter();
+            JSONObject jsonObject=new JSONObject();
+            jsonObject.put("loginstatus","8888");//为获取到userId
+            writer.append(jsonObject.toString());
+//            response.sendRedirect(request.getContextPath()+"/login/index");//返回标志和重定向任选其一
+            return false;
+        }
 //        if (1==1){
 //            response.sendRedirect(request.getContextPath()+"/login/index");
 //        }
-        return true;
+
     }
 
     @Override
