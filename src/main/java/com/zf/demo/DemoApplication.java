05f4fc210252f386d034e01e68a1ca34f20a32b3
dreamerzf <zengief@163.com>
2020-11-05 11:37:12 +0800

    1、心跳保文， 2、双数据源 加上注解@primary  在启动类的@SpringBootApplication添加 (exclude = DataSourceAutoConfiguration.class)避免@bean在项目启动时出错

--- src/main/java/com/zf/demo/DemoApplication.java
+++ src/main/java/com/zf/demo/DemoApplication.java
@@ -2,12 +2,13 @@ package com.zf.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableCaching
 @EnableElasticsearchRepositories(basePackages = "com.zf.demo.usermangeofes.dao")
 public class DemoApplication extends SpringBootServletInitializer {
